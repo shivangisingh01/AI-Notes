c4b51504d968a8a12ae3ea2babdeca57c6059966
Shivangi Singh Pal <105532681+shivangisingh01@users.noreply.github.com>
2025-02-10 11:51:12 +0530

    Updated vite.config.js

--- note-app/vite.config.js
+++ note-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['vmql6d-5173.csb.app'], // Allow the Codesandbox host
+    host: true, // Ensure the server is accessible externally
+  },
 })
